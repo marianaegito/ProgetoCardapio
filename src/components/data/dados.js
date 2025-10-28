18ea6e48cd35fe6d2e0e829bb5405d61d45aacad
Mariana <labsfiap@fiap.com.br>
2025-10-28 08:43:13 -0300

    Criando Projeto Cardapio

---
+++ src/components/data/dados.js
@@ -0,0 +1,51 @@
+const menu =[
+    {
+        id:1,
+        titulo:"Jogo 1",
+        categoria:"acao",
+        preco:200.00,
+        img:"/src/assets/img.jpg",
+        descricao:" descricao do jogo 1"
+    },
+    {
+        id:1,
+        titulo:"Jogo 2",
+        categoria:"acao",
+        preco:170.00,
+        img:"/src/assets/img.jpg",
+        descricao:" descricao do jogo 2"
+    },
+    {
+        id:1,
+        titulo:"Jogo 3",
+        categoria:"acao",
+        preco:210.00,
+        img:"/src/assets/img.jpg",
+        descricao:" descricao do jogo 3"
+    },
+    { 
+        id:1,
+        titulo:"Jogo 4",
+        categoria:"terror",
+        preco:150.00,
+        img:"/src/assets/img.jpg",
+        descricao:" descricao do jogo 4"
+    },
+    {
+        id:1,
+        titulo:"Jogo 5",
+        categoria:"terror",
+        preco:180.00,
+        img:"/src/assets/img.jpg",
+        descricao:" descricao do jogo 5"
+    },
+    {
+        id:1,
+        titulo:"Jogo 6",
+        categoria:"esporye",
+        preco:380.00,
+        img:"/src/assets/img.jpg",
+        descricao:" descricao do jogo 4"
+    }
+]
+export default menu
